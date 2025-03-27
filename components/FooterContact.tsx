58827733d92e154bef85fe3577595ffd234ddfd1
Junseop-Shin <nuclearbomb6518@gmail.com>
2025-03-27 14:48:26 +0900

    [feat] About 페이지 생성 - About/FooterContact 컴포넌트 생성 - ImageCarousel 컴포넌트 생성 - Carousel 관련 수정 필요 - FooterContact 타이틀 수정 필요

---
+++ components/FooterContact.tsx
@@ -0,0 +1,137 @@
+import styled from "styled-components";
+import Image from "next/image";
+import Link from "next/link";
+import { footerList } from "./Footer";
+import FadeUpAnimation from "./FadeUpAnimation";
+
+interface FooterContactProps {
+  bg?: string;
+}
+
+const FooterContactSection = styled.section<{ bg?: string }>`
+  padding: 5rem 2rem;
+  background-color: ${(props) => props.bg ?? "#d9ffe0"};
+`;
+
+const FooterContactContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FooterContactTitle = styled(Image)`
+  flex: 1;
+  transition: opacity 0.3s ease;
+  margin-bottom: 2rem;
+
+  @media (max-width: 768px) {
+    font-size: 2.5rem;
+  }
+`;
+
+const FooterContactBodyRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 1rem 10rem;
+`;
+
+const FooterContactSubtitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FooterContactSubtitle = styled.p`
+  color: rgb(18, 18, 18);
+  font-size: 20px;
+  line-height: 1.8;
+  font-weight: 350;
+`;
+
+const FooterContactButton = styled.button`
+  padding: 1rem 2rem;
+  background-color: transparent;
+  border: 2px solid black;
+  border-radius: 30px;
+  width: 180px;
+  height: 60px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  font-size: 18px;
+  font-weight: 600;
+  align-items: center;
+  text-align: center;
+  align-self: flex-end;
+  margin-bottom: 1rem;
+
+  &:hover {
+    background-color: white;
+  }
+`;
+
+const FooterContactSubtitleSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-top: 4px solid black;
+  border-bottom: 1px solid black;
+  padding: 2rem 0.5rem;
+  width: 48%;
+`;
+
+export default function FooterContact({ bg = "#d9ffe0" }: FooterContactProps) {
+  return (
+    <FooterContactSection bg={bg}>
+      <FooterContactContainer>
+        <FadeUpAnimation>
+          <FooterContactTitle
+            src="/resources/Main_Title.png"
+            alt="Lead with BOLD"
+            width={1200}
+            height={240}
+            priority
+          />
+        </FadeUpAnimation>
+        <FooterContactBodyRow>
+          <FooterContactSubtitleContainer>
+            <FadeUpAnimation>
+              <FooterContactSubtitle>
+                <strong>'대담함으로 앞장서다'</strong>
+              </FooterContactSubtitle>
+              <FooterContactSubtitle>
+                볼드의 비전과 함께 새로운 가능성을 만들어가요!
+              </FooterContactSubtitle>
+              <FooterContactSubtitle>
+                다양한 프로젝트와 협업 문의를 언제든 환영합니다🔥
+              </FooterContactSubtitle>
+            </FadeUpAnimation>
+          </FooterContactSubtitleContainer>
+          <Link href="/contact" passHref>
+            <FooterContactButton>문의하기</FooterContactButton>
+          </Link>
+        </FooterContactBodyRow>
+        <FooterContactBodyRow>
+          <FooterContactSubtitleSection>
+            <FadeUpAnimation>
+              <FooterContactSubtitle>
+                <strong>TEL</strong>
+              </FooterContactSubtitle>
+              <FooterContactSubtitle>{footerList.phone}</FooterContactSubtitle>
+            </FadeUpAnimation>
+          </FooterContactSubtitleSection>
+          <FooterContactSubtitleSection>
+            <FadeUpAnimation>
+              <FooterContactSubtitle>
+                <strong>E-MAIL</strong>
+              </FooterContactSubtitle>
+              <FooterContactSubtitle>{footerList.email}</FooterContactSubtitle>
+            </FadeUpAnimation>
+          </FooterContactSubtitleSection>
+        </FooterContactBodyRow>
+      </FooterContactContainer>
+    </FooterContactSection>
+  );
+}
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,12 +5,17 @@ import Footer from "./Footer";
 interface LayoutProps {
   children: ReactNode;
   showFooter?: boolean;
+  headerColor?: boolean;
 }
 
-export default function Layout({ children, showFooter = true }: LayoutProps) {
+export default function Layout({
+  children,
+  showFooter = true,
+  headerColor = true,
+}: LayoutProps) {
   return (
     <>
-      <Header />
+      <Header headerColor={headerColor} />
       <main>{children}</main>
       {showFooter && <Footer />}
     </>
